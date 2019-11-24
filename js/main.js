1aaf2be54a60fb76af2ef6c8ca90370bc8881d87
Unknown <steppi.thiel@googlemail.com>
2019-11-24 16:35:20 +0100

    Update NEI Module

--- js/main.js
+++ js/main.js
@@ -33,7 +33,6 @@ function checkDlQuery() {
         }
     }
 }
-
 function getElementWithKey(array, key) {
     for(let i = 0; i < array.length; i++) {
         if(array[i].key) {
@@ -45,6 +44,30 @@ function getElementWithKey(array, key) {
     return null;
 }
 checkDlQuery();
+
+function get_browser() {
+    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+        return {name:'IE',version:(tem[1]||'')};
+        }   
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR|Edge\/(\d+)/)
+        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+        }   
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+    return {
+      name: M[0],
+      version: M[1]
+    };
+ }
+
+let browser = get_browser();
+if(browser.name == "Firefox" && browser.version < 68) {
+	alert("Please update firefox to view this page")
+	window.onload = null;
+}
 
 window.onload = () => {
 
