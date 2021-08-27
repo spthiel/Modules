35cf07a9319d859420a47fe8e74e47d79eed2c5a
spthiel <sp4rklingskull@googlemail.com>
2021-08-27 22:09:43 +0200

    Fix download url of codeexporter

--- js/modules.js
+++ js/modules.js
@@ -576,7 +576,7 @@ new Module("CodeExporter")
 .setAuthor("Elspeth")
 .setDescription("Allows to export all currently running scripts into files.")
 .addAction("codeexport([&directory])","Exports all currently running code into the specified directory or macros/exports by default")
-.addDownload(26,"./modules/codeexporter/module_CodeExporter-1.0-26.1-0.15.5-mc1.12.1.jar")
+.addDownload(26,"./modules/codeexporter/module_CodeExporter-1.0-26.1-0.15.4-mc1.12.1.jar")
 .addChangelog(26,"27/aug/21")
     .addLine("Initial release")
     .build();
